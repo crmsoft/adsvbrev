ac7fc66add2cc0007c0312cc7f4377df5d46a319
Akhtem <akhtem.veliliaiev@mediatriple.com>
2019-02-10 14:36:01 +0200

    c

---
+++ config/profile.php
@@ -0,0 +1,43 @@
+<?php 
+
+return [
+
+    // profile play devices selection defaults
+    'devices' => [
+        [
+            'key' => 'pc',
+            'title' => 'PC',
+            'selected' => false
+        ],
+        [
+            'key' => 'playstation_3',
+            'title' => 'Playstation 3',
+            'selected' => false
+        ],
+        [
+            'key' => 'playstation_4',
+            'title' => 'Playstation 4',
+            'selected' => false
+        ],
+        [
+            'key' => 'xbox_360',
+            'title' => 'Xbox 360',
+            'selected' => false
+        ],
+        [
+            'key' => 'nintendo',
+            'title' => 'Nintendo',
+            'selected' => false
+        ],
+        [
+            'key' => 'mobile',
+            'title' => 'Mobile',
+            'selected' => false
+        ],
+        [
+            'key' => 'xbox_one',
+            'title' => 'Xbox One',
+            'selected' => false
+        ]
+    ]
+];
