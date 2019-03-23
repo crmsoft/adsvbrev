e7f9e4bf4e3e508a2618de3d61cc40f315346189
Akhtem <akhtem.veliliaiev@mediatriple.com>
2019-03-23 22:49:19 +0200

    scd

--- public/front/src.7ed060e2.js
+++ public/front/src.7ed060e2.js
@@ -71609,8 +71609,7 @@ var _events = require("./redux/events");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-//const socket = new WebSocket('ws://35.205.191.229/yraMgipTBPDo42aK/?token=' + window.gg.wsc());
-var socket = new WebSocket('ws://127.0.0.1:8181/?token=' + window.gg.wsc());
+var socket = new WebSocket('ws://35.205.191.229/yraMgipTBPDo42aK/?token=' + window.gg.wsc()); //const socket = new WebSocket('ws://127.0.0.1:8181/?token=' + window.gg.wsc());
 
 socket.onclose = function () {//document.location.reload();
 };
