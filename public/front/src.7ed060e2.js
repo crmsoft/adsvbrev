0bbfcbb0c98e60401927255721dc07115e7ddc89
Akhtem <akhtem.veliliaiev@mediatriple.com>
2019-07-14 17:54:01 +0300

    asdc

--- public/front/src.7ed060e2.js
+++ public/front/src.7ed060e2.js
@@ -44604,6 +44604,7 @@ var placeEmoji = function placeEmoji(text) {
       emoji: _react.default.createElement(_emojiMart.Emoji, {
         key: i,
         sheetSize: 20,
+        size: 20,
         emoji: emo,
         set: "google"
       }),
@@ -88723,7 +88724,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42507" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39704" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
